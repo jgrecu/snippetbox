ed0017525f73252176fb53d0b9579c4d345a1ff3
Jeremy Grecu <jgrecu@gmail.com>
2024-05-05 21:02:42 +0200

    Updated the app and added a simple web server.

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snipperbox"))
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	// Use the http.NewServeMux() function to initialize a new ServeMux, then
+	// register the home function as the handler for the "/" URL pattern.
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	// Use the http.ListenAndServe() function to start a new web server. We pass in
+	// two parameters: the TCP network address to listen to (in this case, ":4000")
+	// and the ServeMux we just created. If http.ListenAndServe() returns an error,
+	// we use the log.Fatal() function to log the error message and exit. Note
+	// that any error returned by http.ListenAndServe() is always non-nil.
+	log.Print("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
