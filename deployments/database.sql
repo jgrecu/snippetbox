169d78691b9bed4a4b967ceb5504c0a6bbc3733a
Jeremy Grecu <jgrecu@gmail.com>
2024-06-03 17:06:24 +0200

    Refactored the database.sql file to fix the ALTER USER part

--- deployments/database.sql
+++ deployments/database.sql
@@ -43,6 +43,6 @@ INSERT INTO snippets (title, content, created, expires) VALUES (
 CREATE USER 'web'@'%';
 GRANT SELECT, INSERT, UPDATE, DELETE ON snippetbox.* TO 'web'@'%';
 -- Important: Make sure to swap 'pass' with a password of your own choosing.
-ALTER USER 'web'@'localhost' IDENTIFIED BY 'pass';
+ALTER USER 'web'@'%' IDENTIFIED BY 'pass';
 -- flush privileges;
 FLUSH PRIVILEGES;
