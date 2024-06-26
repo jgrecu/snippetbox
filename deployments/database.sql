e6bd6bd1ed941411a21fb98a84a5aa0a2c3a768a
Jeremy Grecu <jgrecu@gmail.com>
2024-06-26 18:51:43 +0200

    Migrate the database to add the users table for the user authentication

--- deployments/database.sql
+++ deployments/database.sql
@@ -58,3 +58,15 @@ CREATE TABLE sessions (
 
 -- Add an index on the created column.
 CREATE INDEX sessions_expiry_idx ON sessions (expiry);
+
+USE snippetbox;
+
+CREATE TABLE users (
+    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    hashed_password CHAR(60) NOT NULL,
+    created DATETIME NOT NULL
+);
+
+ALTER TABLE users ADD CONSTRAINT users_uc_email UNIQUE (email);
