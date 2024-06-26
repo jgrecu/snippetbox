73ae3daaa8b8aaed9479fcf0eecf70a7cda026ed
Jeremy Grecu <jgrecu@gmail.com>
2024-06-26 18:49:27 +0200

    Adding user authentication - routes setup, changed mav.tmpl

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -13,19 +13,19 @@ func (app *application) routes() http.Handler {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	// Create a new middleware chain containing the middleware specific to our
-	// dynamic application routes. For now, this chain will only contain the
-	// LoadAndSave session middleware but we'll add more to it later.
 	dynamic := alice.New(app.sessionManager.LoadAndSave)
 
-	// Update these routes to use the new dynamic middleware chain followed by
-	// the appropriate handler function. Note that because the alice ThenFunc()
-	// method returns a http.Handler (rather than a http.HandlerFunc) we also
-	// need to switch to registering the route using the mux.Handle() method.
 	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
 	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.snippetView))
 	mux.Handle("GET /snippet/create", dynamic.ThenFunc(app.snippetCreate))
 	mux.Handle("POST /snippet/create", dynamic.ThenFunc(app.snippetCreatePost))
+
+	// Add the five new routes, all of which use our 'dynamic' middleware chain.
+	mux.Handle("GET /user/signup", dynamic.ThenFunc(app.userSignup))
+	mux.Handle("POST /user/signup", dynamic.ThenFunc(app.userSignupPost))
+	mux.Handle("GET /user/login", dynamic.ThenFunc(app.userLogin))
+	mux.Handle("POST /user/login", dynamic.ThenFunc(app.userLoginPost))
+	mux.Handle("POST /user/logout", dynamic.ThenFunc(app.userLogoutPost))
 
 	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
 	return standard.Then(mux)
