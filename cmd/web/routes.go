a1bf391304549a0a6c74ad0dfa0c5d3047d858cd
Jeremy Grecu <jgrecu@gmail.com>
2024-07-23 18:31:35 +0200

    Added embedding static files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,13 +4,21 @@ import (
 	"net/http"
 
 	"github.com/justinas/alice"
+	"snippetbox.jgrecu.eu/ui"
 )
 
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+	// Use the http.FileServerFS() function to create a HTTP handler which
+	// serves the embedded files in ui.Files. It's important to note that our
+	// static files are contained in the "static" folder of the ui.Files
+	// embedded filesystem. So, for example, our CSS stylesheet is located at
+	// "static/css/main.css". This means that we no longer need to strip the
+	// prefix from the request URL -- any requests that start with /static/ can
+	// just be passed directly to the file server and the corresponding static
+	// file will be served (so long as it exists).
+	mux.Handle("GET /static/", http.FileServerFS(ui.Files))
 
 	// Add the authenticate() middleware to the chain.
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
